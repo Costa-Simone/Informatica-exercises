2284dab98c2f0b5299819258259472e2d89552c0
costawski <s.costa.2235@vallauri.edu>
2021-12-01 13:07:33 +0100

    Lezione(01-21-21)

---
+++ ExVettori/ExLibri/Ex9pag115_Solution/n9pag115/ClsVettori.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n9pag115
+{
+    class ClsVettori
+    {
+        public static void ordinamentoCitta(string[] citta, string[] nominativo)
+        {
+            int i = -1;
+            bool scambio;
+            string city;
+            string name;
+
+            do
+            {
+                i++;
+                scambio = false;
+                for (int j = citta.Length - 2; j >= i; j--)
+                {
+                    if (string.Compare(citta[j], citta[j + 1])>0)
+                    {
+                        city = citta[j];
+                        name = nominativo[j];
+                        citta[j] = citta[j + 1];
+                        nominativo[j] = nominativo[j + 1];
+                        citta[j + 1] = city;
+                        nominativo[j + 1] = name;
+                        scambio = true;
+                    }
+                }
+
+            } while (i != citta.Length - 2 && scambio);
+        }
+        internal static void ricercaStudentiDataCitta(string[] nominativo, string[] citta)
+        {
+            Console.Write("Inserisci la città : ");
+            string ct = Console.ReadLine();
+            string stOut = "";
+            int i = 0;
+            bool trovato = false;
+            bool superato = false;
+            int cont = 0;
+            while (!superato && i <= citta.Length - 1)
+            {
+                if (citta[i] == ct)
+                {
+                    stOut += nominativo[i] +"\n";
+                    trovato = true;
+                    i++;
+                    cont++;
+                }
+                else
+                {
+                    if (citta[i].CompareTo(ct) > 0)
+                    {
+                        superato = true;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+            }
+            if (!trovato)
+            {
+                Console.WriteLine("\nCittà non trovata!");
+            }
+            else
+            {
+                Console.WriteLine("\nStudenti provenienti da " + ct+" sono: "+cont);
+                Console.WriteLine("\n" + stOut);
+            }
+        }
+    }
+}
---
+++ ExVettori/ExLibri/Ex9pag115_Solution/n9pag115/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace n9pag115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nome = {"Frank","Dodo","Treppa","Gala","Roxi","Huge","Depo","Magna"};
+            string[] cognome = { "Pier", "Perassi", "Trecco", "Volpin", "Rossa", "Hussein", "Depetris", "Magnano" };
+            string[] citta = { "Barge", "San Martino", "Crocera", "Barge", "San Martino", "Crocera", "Barge", "San Martino" };
+
+            string[] nominativo = new string[nome.Length];
+            for (int i = 0; i < nome.Length; i++)
+            {
+                nominativo[i] = cognome[i] + " " + nome[i];
+                Console.Write("Studente: " + nominativo[i] +"\t"+ " della città : " + citta[i]+ "\n");
+            }
+            ClsVettori.ordinamentoCitta(citta,nominativo);
+            Console.WriteLine();
+            ClsVettori.ricercaStudentiDataCitta(nominativo, citta);
+
+            // per ogni città contare numero di studenti
+            contaStudenti(citta);
+
+            // per ogni città stampare i suoi studenti
+            stampaStudenti(cognome, citta);
+
+            // cercare la città con il maggior numero di Studenti(con parimerito)
+
+            // cercare la città con il maggior numero di Studenti(senza parimerito)
+
+            attesaTasto();
+        }
+        private static void stampaStudenti(string[] cognome, string[] citta)
+        {
+            Array.Resize(ref citta, citta.Length + 1);
+            citta[citta.Length - 1] = "ZZ";
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("\n" + citta[0]);
+            Console.ResetColor();
+
+            for (int i = 0; i < citta.Length - 1; i++)
+            {
+                if (citta[i] == citta[i + 1])
+                {
+                    Console.WriteLine("\n" + cognome[i]);
+                }
+                else
+                {
+                    Console.WriteLine("\n" + cognome[i]);
+                    if (citta[i + 1] != "ZZ")
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("\n" + citta[i + 1]);
+                        Console.ResetColor();
+                    }
+                }
+            }
+        }
+        public static void attesaTasto()
+        {
+            Console.WriteLine("\n(premi un tasto per continuare)");
+            Console.ReadKey();
+        }
+        private static void contaStudenti(string[] citta)
+        {
+            Array.Resize(ref citta, citta.Length + 1);
+            citta[citta.Length - 1] = "ZZ";
+
+            int cont = 1;
+
+            for (int i = 0; i < citta.Length - 1; i++)
+            {
+                if (citta[i] == citta[i + 1])
+                {
+                    cont++;
+                }
+                else
+                {
+                    Console.WriteLine("\n" + citta[i] + " " + cont.ToString());
+                    cont = 1;
+                }
+            }
+        }
+    }
+}
