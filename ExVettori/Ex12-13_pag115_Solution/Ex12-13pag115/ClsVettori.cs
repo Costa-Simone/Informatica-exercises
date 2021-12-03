8708b64adfb5e99eea70387b6ed8c6934f9c4248
costawski <s.costa.2235@vallauri.edu>
2021-12-03 17:23:17 +0100

    Compito(03-12-21)

---
+++ ExVettori/Ex12-13_pag115_Solution/Ex12-13pag115/ClsVettori.cs
@@ -0,0 +1,150 @@
+﻿using System;
+
+namespace Ex12_13pag115
+{
+    class ClsVettori
+    {
+        static Random rnd = new Random();
+
+        public static void ricerca_seq1(int[] v, int x)
+        {
+            int i = 0;
+
+            while (v[i] != x && i != v.Length - 1)
+                i++;
+            if (v[i] == x)
+                Console.WriteLine("Elemento trovato in pos: " + i.ToString());
+            else
+                Console.WriteLine("Elemento non trovato");
+        }
+        public static void ricerca_seq2(int[] v, int x)
+        {
+            bool trovato = false;
+            for (int i = 0; i < v.Length; i++)
+            {
+                if (v[i] == x)
+                {
+                    Console.WriteLine("Elemento trovato in pos: " + i.ToString());
+                    trovato = true;
+                }
+            }
+            if (!trovato)
+                Console.WriteLine("Elemento non trovato");
+        }
+        public static void caricaVetRndOrdinatoSenzaRip(int[]v)
+        {
+            //variabili locali
+            
+            int max = 10;
+
+            v[0] = rnd.Next(1, max);
+            for (int i = 1; i < v.Length; i++)
+            {
+                max = v[i - 1] + 1;
+                v[i] = rnd.Next(max, max + 10);
+            }
+        }
+        public static void ricerca_seq4(int[] v, int x)
+        {
+            int i = 0;
+            bool trovato= false;
+            bool superato= false;
+
+            while(!superato && i<=(v.Length))
+            {
+                if(v[i]==x)
+                {
+                    Console.WriteLine("Elemento trovato in pos: " + i.ToString());
+                    trovato = true;
+                    i += 1;
+                }
+                else
+                {
+                    if (v[i] > x)
+                        superato = true;
+                    else
+                        i += 1;
+                }
+            }
+            if(!trovato)
+                Console.WriteLine("Elemento non trovato");
+        }
+        public static void caricaVetRndOrdinatoConRipet(int[] v)
+        {
+            //variabili locali
+
+            int x;
+            int max=250;
+
+            v[0] = rnd.Next(1, max+1);
+            for (int i = 1; i < v.Length; i++)
+            {
+                do
+                    x = rnd.Next(1, max+1);
+                while (x < v[i - 1]);
+                v[i] = x;
+            }
+        }
+        public static void ricerca_seq3(int[] v, int x)
+        {
+            int i = 0;
+
+            while (v[i] < x && i != v.Length - 1)
+                i++;
+            if (v[i] == x)
+                Console.WriteLine("Elemento trovato in pos: " + i.ToString());
+            else
+                Console.WriteLine("Elemento non trovato");
+        }
+        public static void caricaVetRndSenzaRip(int[] v)
+        {
+            //variabili locali
+
+            int x, j;
+
+            v[0] = rnd.Next(1, 100);
+            for (int i = 1; i < v.Length; i++)
+            {
+                do
+                {
+                    x = rnd.Next(1, 100);
+                    j = 0;
+                    while (v[j] != x && j != i - 1)
+                        j++;
+                }
+                while (v[j] == x);
+                v[i] = x;
+            }
+        }
+        public static void attesaTasto()
+        {
+            Console.WriteLine("\n(premi un tasto per continuare)");
+            Console.ReadKey();
+        }
+
+        public static int inputDimensione(string messaggio)
+        {
+            int n;
+            do
+            {
+                Console.Write(messaggio);
+                if (!int.TryParse(Console.ReadLine(), out n))
+                    Console.WriteLine("Devi inserire un numero > 0!");
+            }
+            while (n <= 0);
+            return n;
+        }
+        public static void caricaVet(int[] v)
+        {
+            for (int i = 0; i < v.Length; i++)
+                v[i] = rnd.Next(1, 10);
+        }
+        public static void stampaVet(int[] v, string nome)
+        {
+            Console.Write(nome + ": ");
+            for (int i = 0; i < v.Length; i++)
+                Console.Write(v[i].ToString().PadRight(6));
+        }
+
+    }
+}
---
+++ ExVettori/Ex12-13_pag115_Solution/Ex12-13pag115/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex12_13pag115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] caseEditrici = { "Feltri", "Monda", "Adelphi", "Adelphi", "Feltri", "Feltri", "Monda" };
+            string[] nomiLibri = { "Dorian", "princi", "Guerra", "Odisse", "1984", "Il ros", "Faust" };
+            string[] generi = { "Horror", "Fantasy", "Triller", "Triller", "Fantasy", "Fantasy", "Horror" };
+
+            stampaTab(caseEditrici, nomiLibri, generi);
+
+            ricercaLibri(caseEditrici, nomiLibri, generi);
+
+            ordinaSelezione(caseEditrici, nomiLibri, generi);
+            stampaTab(caseEditrici, nomiLibri, generi);
+
+            ricercaLibri2(caseEditrici, nomiLibri, generi);
+
+            attesaTasto();
+        }
+        private static void stampaTab(string[] caseEditrici, string[] nomiLibri, string[] generi)
+        {
+            for (int i = 0; i < nomiLibri.Length; i++)
+            {
+                Console.Write("Casa editrice: " + caseEditrici[i] + "\t" + " di nome:\t " + nomiLibri[i] + "\t\t genere:\t\t" + generi[i] + "\n");
+            }
+        }
+        public static void attesaTasto()
+        {
+            Console.WriteLine("\n(premi un tasto per continuare)");
+            Console.ReadKey();
+        }
+        public static void ricercaLibri(string[] caseEditrici, string[] nomiLibri, string[] generi)
+        {
+            Console.WriteLine("\nInserisci la casa editrice: ");
+            string editrice = Console.ReadLine();
+            Console.WriteLine("\nInserisci il genere: ");
+            string genere = Console.ReadLine();
+
+            int cont = 0;
+
+            for (int i = 0; i < caseEditrici.Length; i++)
+            {
+                if (caseEditrici[i] == editrice && generi[i] == genere)
+                {
+                    cont++;
+                }
+            }
+
+            Console.WriteLine("\nIl numero di libri e': " + cont.ToString());
+        }
+        internal static void ordinaSelezione(string[] caseEditrici, string[] nomiLibri, string[] generi)
+        {
+            int PosMin;
+            string Editrici, nomi, genere;
+
+            for (int i = 0; i <= caseEditrici.Length - 2; i++)
+            {
+                PosMin = i;
+
+                for (int j = i + 1; j <= caseEditrici.Length - 1; j++)
+                {
+                    if (string.Compare(caseEditrici[PosMin], caseEditrici[j]) > 0)
+                    {
+                        PosMin = j;
+                    }
+                }
+
+                if (PosMin != i)
+                {
+                    Editrici = caseEditrici[i];
+                    nomi = nomiLibri[i];
+                    genere = generi[i];
+                    caseEditrici[i] = caseEditrici[PosMin];
+                    nomiLibri[i] = nomiLibri[PosMin];
+                    generi[i] = generi[PosMin];
+                    caseEditrici[PosMin] = Editrici;
+                    nomiLibri[PosMin] = nomi;
+                    generi[PosMin] = genere;
+                }
+            }
+        }
+        public static void ricercaLibri2(string[] caseEditrici, string[] nomiLibri, string[] generi)
+        {
+            Console.WriteLine("\nInserisci la casa editrice: ");
+            string editrice = Console.ReadLine();
+            Console.WriteLine("\nInserisci il genere: ");
+            string genere = Console.ReadLine();
+
+            int i = 0, cont = 0;
+            bool superato = false;
+
+            while (!superato && i <= caseEditrici.Length)
+            {
+                if (caseEditrici[i] == editrice && generi[i] == genere)
+                {
+                    cont++;
+                    i += 1;
+                }
+                else
+                {
+                    if (string.Compare(caseEditrici[i], editrice) > 0)
+                    {
+                        superato = true;
+                    }
+                    else
+                    {
+                        i += 1;
+                    }
+                }
+            }
+
+            Console.WriteLine("\nIl numero di libri e': " + cont.ToString());
+        }
+    }
+}
