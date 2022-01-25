1aef34b707aa27727c02c72d87d82af8ad9d3265
costawski <s.costa.2235@vallauri.edu>
2022-01-25 11:43:23 +0100

    Verifica(25-01-22)

---
+++ ExVettori/Costa_25_01_22/Costa_25_01_22/ClsVerifica.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Costa_25_01_22
+{
+    class ClsVerifica
+    {
+        static Random rnd = new Random();
+        internal static void ordinaStudenti(string[] studenti, string[] materie, int[] voti)
+        {
+            int PosMin, voto;
+            string studente, materia;
+
+            for (int i = 0; i <= studenti.Length - 2; i++)
+            {
+                PosMin = i;
+
+                for (int j = i + 1; j <= studenti.Length - 1; j++)
+                {
+                    if (string.Compare(studenti[PosMin], studenti[j]) > 0)
+                    {
+                        PosMin = j;
+                    }
+                }
+
+               if (PosMin != i)
+               {
+                    studente = studenti[i];
+                    materia = materie[i];
+                    voto = voti[i];
+                    studenti[i] = studenti[PosMin];
+                    materie[i] = materie[PosMin];
+                    voti[i] = voti[PosMin];
+                    studenti[PosMin] = studente;
+                    materie[PosMin] = materia;
+                    voti[PosMin] = voto;
+               }
+            }
+       
+        }
+        internal static void calcolaMedia(string[] studenti, string[] materie, int[] voti)
+        {
+            Array.Resize(ref studenti, studenti.Length + 1);
+            Array.Resize(ref materie, materie.Length + 1);
+            Array.Resize(ref voti, voti.Length + 1);
+            studenti[studenti.Length - 1] = "ZZ";
+            materie[studenti.Length - 1] = "ZZ";
+
+            Console.Write("\nInserisci materia: ");
+            string materia = Convert.ToString(Console.ReadLine());
+
+            int cont = 0;
+            float media = 0;
+            string aus = studenti[0];
+
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                if (studenti[i] == aus)
+                {
+                    if (materie[i] == materia)
+                    {
+                        media += voti[i];
+                        cont++;
+                    }
+                }
+                else
+                {
+                    if (cont != 0)
+                    {
+                        Console.WriteLine(studenti[i - 1] + ": media voto " + (media / cont).ToString());
+                    }
+                    else
+                    {
+                        Console.WriteLine(studenti[i - 1] + ": nessun voto");
+                    }
+                    
+                    aus = studenti[i];
+                    cont = 0;
+                    media = 0;
+                }
+            }
+        }
+        internal static void stampaTab(string[] studenti, int[] voti, string[] materie)
+        {
+            for (int i = 0; i < studenti.Length; i++)
+            {
+                Console.Write("Studente: " + studenti[i] + "\t" + "di voto:\t " + voti[i] + "\t\t" + "della materia:\t " + materie[i] + "\n");
+            }
+        }
+        internal static void assegnaVoti(int[] voti)
+        {
+            for (int i = 0; i < voti.Length; i++)
+            {
+                voti[i] = rnd.Next(0, 11);
+            }
+        }
+    }
+}
---
+++ ExVettori/Costa_25_01_22/Costa_25_01_22/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Costa_25_01_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] studenti = { "Servetti", "Vaschetto", "Vaschetto", "Cambieri", "Servetti", "Barbero", "Barbero", "Servetti", 
+                "Vaschetto", "Vaschetto", "Cambieri", "Servetti", "Barbero", "Barbero" };
+            string[] materie = { "Storia", "Matematica", "Informatica", "Inglese", "Matematica", "Inglese", "Storia", "Informatica",
+                "Inglese", "Matematica", "Inglese", "Storia", "Matematica", "Matematica" };
+            int[] voti = new int[studenti.Length];
+
+            ClsVerifica.assegnaVoti(voti);
+            //ClsVerifica.stampaTab(studenti, voti, materie);
+            ClsVerifica.ordinaStudenti(studenti, materie, voti);
+            ClsVerifica.stampaTab(studenti, voti, materie);
+            ClsVerifica.calcolaMedia(studenti, materie, voti);
+
+            Console.ReadKey();
+        }
+    }
+}
