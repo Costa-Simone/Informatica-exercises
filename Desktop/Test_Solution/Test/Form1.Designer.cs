f5b4a0f09f0ba4f41b39e2a8ad2e85e036f512c3
costawski <s.costa.2235@vallauri.edu>
2022-02-22 11:37:01 +0100

    Lezione(22-02-22)

--- Desktop/Test_Solution/Test/Form1.Designer.cs
+++ Desktop/Test_Solution/Test/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
-namespace WindowsFormTest
+namespace Test
 {
-    partial class FormMain
+    partial class Form1
     {
         /// <summary>
         /// Variabile di progettazione necessaria.
@@ -29,24 +29,39 @@ namespace WindowsFormTest
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(242, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chiudi programma";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(374, 175);
+            this.button2.Location = new System.Drawing.Point(332, 174);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 0;
+            this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // FormMain
+            // Form1
             // 
-            this.ClientSize = new System.Drawing.Size(736, 420);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.button2);
-            this.Name = "FormMain";
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
             this.ResumeLayout(false);
 
         }
--- Desktop/Test_Solution/Test/Form1.cs
+++ Desktop/Test_Solution/Test/Form1.cs
@@ -8,16 +8,21 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace WindowsFormTest
+namespace Test
 {
-    public partial class FormMain : Form
+    public partial class Form1 : Form
     {
-        public FormMain()
+        public Form1()
         {
             InitializeComponent();
         }
 
-        private void btnChiudi_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ollare");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
         {
             Close();
         }
