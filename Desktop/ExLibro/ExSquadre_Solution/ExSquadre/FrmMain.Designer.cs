989dc240109e987b276981213926017566de26b8
costawski <s.costa.2235@vallauri.edu>
2022-03-28 17:56:34 +0200

    Compito(28-03-22)

---
+++ Desktop/ExLibro/ExSquadre_Solution/ExSquadre/FrmMain.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace ExSquadre
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAtleti = new System.Windows.Forms.DataGridView();
+            this.dgvNazioni = new System.Windows.Forms.DataGridView();
+            this.dgvTeam = new System.Windows.Forms.DataGridView();
+            this.btnTopSquadre = new System.Windows.Forms.Button();
+            this.lblTopSquadre = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAtleti)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNazioni)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTeam)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvAtleti
+            // 
+            this.dgvAtleti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAtleti.Location = new System.Drawing.Point(12, 12);
+            this.dgvAtleti.Name = "dgvAtleti";
+            this.dgvAtleti.Size = new System.Drawing.Size(100, 150);
+            this.dgvAtleti.TabIndex = 0;
+            // 
+            // dgvNazioni
+            // 
+            this.dgvNazioni.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvNazioni.Location = new System.Drawing.Point(118, 12);
+            this.dgvNazioni.Name = "dgvNazioni";
+            this.dgvNazioni.Size = new System.Drawing.Size(100, 150);
+            this.dgvNazioni.TabIndex = 1;
+            // 
+            // dgvTeam
+            // 
+            this.dgvTeam.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTeam.Location = new System.Drawing.Point(224, 12);
+            this.dgvTeam.Name = "dgvTeam";
+            this.dgvTeam.Size = new System.Drawing.Size(100, 150);
+            this.dgvTeam.TabIndex = 2;
+            // 
+            // btnTopSquadre
+            // 
+            this.btnTopSquadre.Location = new System.Drawing.Point(330, 12);
+            this.btnTopSquadre.Name = "btnTopSquadre";
+            this.btnTopSquadre.Size = new System.Drawing.Size(75, 65);
+            this.btnTopSquadre.TabIndex = 3;
+            this.btnTopSquadre.Text = "Top Squadre";
+            this.btnTopSquadre.UseVisualStyleBackColor = true;
+            this.btnTopSquadre.Click += new System.EventHandler(this.btnTopSquadre_Click);
+            // 
+            // lblTopSquadre
+            // 
+            this.lblTopSquadre.Location = new System.Drawing.Point(411, 26);
+            this.lblTopSquadre.Name = "lblTopSquadre";
+            this.lblTopSquadre.Size = new System.Drawing.Size(100, 23);
+            this.lblTopSquadre.TabIndex = 4;
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 530);
+            this.Controls.Add(this.lblTopSquadre);
+            this.Controls.Add(this.btnTopSquadre);
+            this.Controls.Add(this.dgvTeam);
+            this.Controls.Add(this.dgvNazioni);
+            this.Controls.Add(this.dgvAtleti);
+            this.Name = "FrmMain";
+            this.Text = "Squadre";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAtleti)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNazioni)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTeam)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAtleti;
+        private System.Windows.Forms.DataGridView dgvNazioni;
+        private System.Windows.Forms.DataGridView dgvTeam;
+        private System.Windows.Forms.Button btnTopSquadre;
+        private System.Windows.Forms.Label lblTopSquadre;
+    }
+}
+
---
+++ Desktop/ExLibro/ExSquadre_Solution/ExSquadre/FrmMain.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace ExSquadre
+{
+    public partial class FrmMain : Form
+    {
+        static string[] atleti = new string[]
+            {
+                "AA", "BB", "CC", "DD", "EE", "FF",  "GG", "HH", "II", "LL","MM", "NN","OO", "PP", "QQ", "RR", "SS", "TT","UU", "VV", "ZZ"};
+        static string[] nazioni = new string[]
+                    {
+                "ITA", "FRA", "GBR", "USA", "USA", "GBR", "ITA", "FRA", "GBR", "USA", "ITA", "ITA","ITA", "FRA", "GBR", "USA", "ITA", "FRA", "USA", "USA", 
+                        "USA"};
+        static string[] team = new string[]
+                    {
+                "T2", "T5", "T4", "T2", "T4", "T3", "T3", "T1", "T1", "T2", "T5", "T4", "T4", "T1", "T3", "T3", "T5", "T4","T2", "T5", "T4" };
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+            settaDgv(atleti, dgvAtleti);
+            settaDgv(nazioni, dgvNazioni);
+            settaDgv(team, dgvTeam);
+            visualizzaDgv(dgvAtleti, atleti);
+            visualizzaDgv(dgvNazioni, nazioni);
+            visualizzaDgv(dgvTeam, team);
+        }
+        private void visualizzaDgv(DataGridView dgv, string[] dati)
+        {
+            for (int i = 0; i < dati.Length; i++)
+            {
+                dgv.Rows[i].Cells[0].Value = dati[i];
+            }
+        }
+        private void settaDgv(string[] dati, DataGridView dgv)
+        {
+            dgv.ColumnCount = 1;
+            dgv.RowCount = dati.Length;
+            dgv.RowHeadersVisible = false;
+            dgv.ColumnHeadersVisible = false;
+            dgv.Height = dati.Length * 22;
+            dgv.ScrollBars = ScrollBars.None;
+            dgv.DefaultCellStyle.SelectionBackColor = Color.Transparent;
+            dgv.DefaultCellStyle.SelectionForeColor = dgv.DefaultCellStyle.ForeColor;
+        }
+        private void btnTopSquadre_Click(object sender, EventArgs e)
+        {
+            int PosMin, cont = 0, contMax = -1;
+            string ausS, teamMax = "";
+
+            for (int i = 0; i <= team.Length - 2; i++)
+            {
+                PosMin = i;
+
+                for (int j = i + 1; j <= team.Length - 1; j++)
+                {
+                    if (string.Compare(team[PosMin], team[j]) > 0)
+                    {
+                        PosMin = j;
+                    }
+                }
+
+                if (PosMin != i)
+                {
+                    ausS = team[i];
+                    team[i] = team[PosMin];
+                    team[PosMin] = ausS;
+
+                    ausS = atleti[i];
+                    atleti[i] = atleti[PosMin];
+                    atleti[PosMin] = ausS;
+
+                    ausS = nazioni[i];
+                    nazioni[i] = nazioni[PosMin];
+                    nazioni[PosMin] = ausS;
+                }
+            }
+
+            visualizzaDgv(dgvAtleti, atleti);
+            visualizzaDgv(dgvNazioni, nazioni);
+            visualizzaDgv(dgvTeam, team);
+
+            string naz = Interaction.InputBox("Inserisci nazione: ");
+
+            if (nazioni[0] == naz)
+            {
+                cont++;
+            }
+
+            for (int i = 1; i < team.Length; i++)
+            {
+                if (team[i] == team[i - 1])
+                {
+                    if (nazioni[i] == naz)
+                    {
+                        cont++;
+                    }
+                }
+                else
+                {
+                    if (cont > contMax)
+                    {
+                        contMax = cont;
+                        teamMax = team[i - 1];
+                        cont = 0;
+                    }
+                }
+            }
+
+            lblTopSquadre.Text = "team " + teamMax.ToString() + " (" + contMax.ToString() + " atleti)";
+        }
+    }
+}
