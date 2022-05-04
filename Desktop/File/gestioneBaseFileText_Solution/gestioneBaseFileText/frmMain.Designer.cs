7cb90aaacd28e50af92c8de9c305ac5f7b5613ae
costawski <s.costa.2235@vallauri.edu>
2022-05-04 13:07:36 +0200

    Lezione(04-05-22)

---
+++ Desktop/File/gestioneBaseFileText_Solution/gestioneBaseFileText/frmMain.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace gestioneBaseFileText
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeggiDaFile = new System.Windows.Forms.Button();
+            this.btnScriviSuFile = new System.Windows.Forms.Button();
+            this.dgvLibri = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibri)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeggiDaFile
+            // 
+            this.btnLeggiDaFile.Location = new System.Drawing.Point(12, 25);
+            this.btnLeggiDaFile.Name = "btnLeggiDaFile";
+            this.btnLeggiDaFile.Size = new System.Drawing.Size(96, 40);
+            this.btnLeggiDaFile.TabIndex = 0;
+            this.btnLeggiDaFile.Text = "LEGGI DA FILE";
+            this.btnLeggiDaFile.UseVisualStyleBackColor = true;
+            this.btnLeggiDaFile.Click += new System.EventHandler(this.btnLeggiDaFile_Click);
+            // 
+            // btnScriviSuFile
+            // 
+            this.btnScriviSuFile.Location = new System.Drawing.Point(12, 71);
+            this.btnScriviSuFile.Name = "btnScriviSuFile";
+            this.btnScriviSuFile.Size = new System.Drawing.Size(96, 40);
+            this.btnScriviSuFile.TabIndex = 1;
+            this.btnScriviSuFile.Text = "SCRIVI SU FILE";
+            this.btnScriviSuFile.UseVisualStyleBackColor = true;
+            this.btnScriviSuFile.Click += new System.EventHandler(this.btnScriviSuFile_Click);
+            // 
+            // dgvLibri
+            // 
+            this.dgvLibri.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvLibri.Location = new System.Drawing.Point(114, 12);
+            this.dgvLibri.Name = "dgvLibri";
+            this.dgvLibri.Size = new System.Drawing.Size(245, 247);
+            this.dgvLibri.TabIndex = 2;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(381, 272);
+            this.Controls.Add(this.dgvLibri);
+            this.Controls.Add(this.btnScriviSuFile);
+            this.Controls.Add(this.btnLeggiDaFile);
+            this.Name = "frmMain";
+            this.Text = "Gestione base file Libri.txt";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvLibri)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeggiDaFile;
+        private System.Windows.Forms.Button btnScriviSuFile;
+        private System.Windows.Forms.DataGridView dgvLibri;
+    }
+}
+
---
+++ Desktop/File/gestioneBaseFileText_Solution/gestioneBaseFileText/frmMain.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //NOTA BENE
+
+namespace gestioneBaseFileText
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            settaDgv(dgvLibri);
+        }
+        private void settaDgv(DataGridView dgv)
+        {
+            dgv.ColumnCount = 4;
+            dgv.Columns[0].HeaderText = "Titolo";
+            dgv.Columns[1].HeaderText = "Autore";
+            dgv.Columns[2].HeaderText = "Anno";
+            dgv.Columns[3].HeaderText = "Prezzo";
+            dgv.AutoResizeColumns();
+            dgv.RowHeadersVisible = false;
+            dgv.ScrollBars = ScrollBars.Vertical;
+        }
+        private void btnLeggiDaFile_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("libri.txt");
+            int i = 0;
+            string s;
+            string[] v = new string[4]; //per splittare
+
+            while (sr.Peek() > -1) //finche' non finisco file
+            {
+                s = sr.ReadLine(); //legge una linea del file
+                v = s.Split(';');
+
+                scriviSuDgv(i, v, dgvLibri);
+
+                i++;
+            }
+        }
+        private void scriviSuDgv(int i, string[] v, DataGridView dgv)
+        {
+            dgv.Rows.Add();
+
+            dgv.Rows[i].Cells[0].Value = v[0];
+            dgv.Rows[i].Cells[1].Value = v[1];
+            dgv.Rows[i].Cells[2].Value = v[2];
+            dgv.Rows[i].Cells[3].Value = v[3];
+        }
+        private void btnScriviSuFile_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("libri.txt", false);
+            //false = scovrascrivo il file
+            //true = appende nuovi dati
+            string s;
+
+            for (int i = 0; i < dgvLibri.RowCount - 1; i++)
+            {
+                s = "";
+
+                for (int j = 0; j < dgvLibri.ColumnCount; j++)
+                {
+                    if (j != dgvLibri.ColumnCount - 1)
+                    {
+                        s += dgvLibri.Rows[i].Cells[j].Value + ";";
+                    }
+                    else
+                    {
+                        s += dgvLibri.Rows[i].Cells[j].Value;
+                    }
+                }
+
+                sw.WriteLine(s);
+            }
+
+            sw.Close();
+        }
+    }
+}
