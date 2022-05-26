facf13c176e3a6fbce2b6f4ff2ea52791b5288b7
costawski <s.costa.2235@vallauri.edu>
2022-05-26 12:29:51 +0200

    Lezione(26-05-22)

---
+++ Desktop/Ricorsione_Solution/Ricorsione/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace Ricorsione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFattoriale = new System.Windows.Forms.Button();
+            this.btnProdotto = new System.Windows.Forms.Button();
+            this.btnFibonacci = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFattoriale
+            // 
+            this.btnFattoriale.Location = new System.Drawing.Point(24, 12);
+            this.btnFattoriale.Name = "btnFattoriale";
+            this.btnFattoriale.Size = new System.Drawing.Size(75, 23);
+            this.btnFattoriale.TabIndex = 0;
+            this.btnFattoriale.Text = "Fattoriale";
+            this.btnFattoriale.UseVisualStyleBackColor = true;
+            this.btnFattoriale.Click += new System.EventHandler(this.btnFattoriale_Click);
+            // 
+            // btnProdotto
+            // 
+            this.btnProdotto.Location = new System.Drawing.Point(24, 41);
+            this.btnProdotto.Name = "btnProdotto";
+            this.btnProdotto.Size = new System.Drawing.Size(75, 23);
+            this.btnProdotto.TabIndex = 1;
+            this.btnProdotto.Text = "Prodotto";
+            this.btnProdotto.UseVisualStyleBackColor = true;
+            this.btnProdotto.Click += new System.EventHandler(this.btnProdotto_Click);
+            // 
+            // btnFibonacci
+            // 
+            this.btnFibonacci.Location = new System.Drawing.Point(24, 70);
+            this.btnFibonacci.Name = "btnFibonacci";
+            this.btnFibonacci.Size = new System.Drawing.Size(75, 23);
+            this.btnFibonacci.TabIndex = 2;
+            this.btnFibonacci.Text = "Fibonacci";
+            this.btnFibonacci.UseVisualStyleBackColor = true;
+            this.btnFibonacci.Click += new System.EventHandler(this.btnFibonacci_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(120, 115);
+            this.Controls.Add(this.btnFibonacci);
+            this.Controls.Add(this.btnProdotto);
+            this.Controls.Add(this.btnFattoriale);
+            this.Name = "Form1";
+            this.Text = "Ricorsione";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFattoriale;
+        private System.Windows.Forms.Button btnProdotto;
+        private System.Windows.Forms.Button btnFibonacci;
+    }
+}
+
---
+++ Desktop/Ricorsione_Solution/Ricorsione/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Ricorsione
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnFattoriale_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(Interaction.InputBox("Inserisci n"));
+
+            MessageBox.Show("Il fattoriale di " + n + " è " + fatt(n));
+        }
+        private int fatt(int n)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n * fatt(n - 1);
+            }
+        }
+        private void btnProdotto_Click(object sender, EventArgs e)
+        {
+            int a = int.Parse(Interaction.InputBox("Inserisci il moltiplicando"));
+            int b = int.Parse(Interaction.InputBox("Inserisci il moltiplicatore"));
+
+            MessageBox.Show("Il prodotto di " + a + " per " + b + " è " + prod(a, b));
+        }
+        private int prod(int a, int b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return a + prod(a, b - 1);
+            }
+        }
+        private void btnFibonacci_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(Interaction.InputBox("Inserisci il moltiplicando"));
+
+            MessageBox.Show("L' " + n + "simo valore di Fibonacci è " + fib(n));
+        }
+        private int fib(int n)
+        {
+            if (n < 2)
+            {
+                return n;
+            }
+            else
+            {
+                return fib(n - 1) + fib(n - 2);
+            }
+        }
+    }
+}
