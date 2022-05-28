76f4a477768c25115b70fe81e01e7aeeb0d24fa6
costawski <s.costa.2235@vallauri.edu>
2022-05-28 10:55:55 +0200

    Lezione(28-05-22)

---
+++ Desktop/Poli_Solution/Poli/FrmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Poli
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSuperEnalotto = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSuperEnalotto
+            // 
+            this.btnSuperEnalotto.Location = new System.Drawing.Point(12, 12);
+            this.btnSuperEnalotto.Name = "btnSuperEnalotto";
+            this.btnSuperEnalotto.Size = new System.Drawing.Size(105, 42);
+            this.btnSuperEnalotto.TabIndex = 0;
+            this.btnSuperEnalotto.Text = "SuperEnalotto";
+            this.btnSuperEnalotto.UseVisualStyleBackColor = true;
+            this.btnSuperEnalotto.Click += new System.EventHandler(this.btnSuperEnalotto_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(125, 68);
+            this.Controls.Add(this.btnSuperEnalotto);
+            this.Name = "FrmMain";
+            this.Text = "Alcuni esami del poli";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSuperEnalotto;
+    }
+}
+
---
+++ Desktop/Poli_Solution/Poli/FrmMain.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+
+namespace Poli
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnSuperEnalotto_Click(object sender, EventArgs e)
+        {
+            int[] numeri = new int[6];
+
+            caricaNumeri(numeri);
+            cercaVincitori(numeri, "giocate.txt");
+        }
+        private void cercaVincitori(int[] numeri, string nf)
+        {
+            StreamWriter sw = new StreamWriter("vincite.txt", false);
+            bool trovato = false;
+            string output = "";
+            int[] contatori = new int[5];
+
+            foreach (string giocate in File.ReadLines(nf))
+            {
+                int cont = 0;
+                int[] scommesse = new int[6];
+                string[] campi = new string[7];
+
+                campi = giocate.Split(' ');
+
+                for (int i = 0; i < numeri.Length; i++)
+                {
+                    scommesse[i] = int.Parse(campi[i + 1]);
+                }
+
+                for (int i = 0; i < numeri.Length; i++)
+                {
+                    if (Array.IndexOf(scommesse, numeri[i]) != -1)
+                    {
+                        cont++;
+                    }
+                }
+
+                if (cont >= 2)
+                {
+                    sw.WriteLine(campi[0] + " " + cont);
+
+                    trovato = true;
+                    contatori[cont - 2]++;
+                }
+            }
+
+            sw.Close();
+
+            if (!trovato)
+            {
+                File.Delete("vincite.txt");
+                MessageBox.Show("Nessun vincitore");
+            }
+            else
+            {
+                for (int i = 4; i >= 0; i--)
+                {
+                    output += "Numero di " + (i + 2) + ": " + contatori[i] + "\n";
+                }
+
+                MessageBox.Show("File creato\n" + output);
+            }
+        }
+        private void caricaNumeri(int[] numeri)
+        {
+            for (int i = 0; i < numeri.Length; i++)
+            {
+                numeri[i] = int.Parse(Interaction.InputBox("Inserisci un valore"));
+            }
+        }
+    }
+}
