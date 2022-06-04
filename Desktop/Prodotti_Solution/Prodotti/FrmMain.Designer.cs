e9a66f947736e33bf1f646169843230760629e1a
costawski <s.costa.2235@vallauri.edu>
2022-06-04 10:53:18 +0200

    Lezione(04-06-22)

---
+++ Desktop/Prodotti_Solution/Prodotti/FrmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Prodotti
+{
+    partial class FrmMain
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCercaMax = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCercaMax
+            // 
+            this.btnCercaMax.Location = new System.Drawing.Point(12, 12);
+            this.btnCercaMax.Name = "btnCercaMax";
+            this.btnCercaMax.Size = new System.Drawing.Size(75, 23);
+            this.btnCercaMax.TabIndex = 0;
+            this.btnCercaMax.Text = "Cerca Max";
+            this.btnCercaMax.UseVisualStyleBackColor = true;
+            this.btnCercaMax.Click += new System.EventHandler(this.btnCercaMax_Click);
+            // 
+            // FrmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnCercaMax);
+            this.Name = "FrmMain";
+            this.Text = "Prodotti";
+            this.Load += new System.EventHandler(this.FrmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCercaMax;
+    }
+}
+
---
+++ Desktop/Prodotti_Solution/Prodotti/FrmMain.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Prodotti
+{
+    public partial class FrmMain : Form
+    {
+        public struct categoria
+        {
+            public string tipo;
+            public float punteggio;
+        }
+        public struct nazione
+        {
+            public string nome;
+            public float punteggio;
+        }
+
+        categoria[] categorie = new categoria[20];
+        nazione[] nazioni = new nazione[20];
+
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+        private void FrmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnCercaMax_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Il prodotto vincente è " + prodottoVincente("prodotti.txt"));
+
+            int nCategorie = caricaCategorie(categorie, "prodotti.txt");
+            int nNazioni = caricaNazioni(nazioni, "prodotti.txt");
+
+            ordina(nazioni, nNazioni);
+            stampaClassifica(nazioni, nNazioni);
+            ordina(categorie, nCategorie);
+            stampaClassifica(categorie, nCategorie);
+        }
+        private void stampaClassifica(nazione[] nazioni, int nNazioni)
+        {
+            throw new NotImplementedException();
+        }
+        private void ordina(nazione[] nazioni, int nNazioni)
+        {
+            
+        }
+        private string prodottoVincente(string nf)
+        {
+            string[] campi = new string[9];
+            float totale, max = 0;
+            string prodotto = "";
+
+            foreach (string categoria in File.ReadLines(nf))
+            {
+                campi = categoria.Split(' ');
+                totale = calcolaTotale(campi);
+
+                if (totale > max)
+                {
+                    max = totale;
+                    prodotto = campi[0] + " " + campi[1] + "-" + max.ToString();
+                }
+            }
+
+            return prodotto;
+        }
+        private int caricaNazioni(nazione[] nazioni, string v)
+        {
+            string[] campi = new string[9], aus = new string[20];
+            int i = 0;
+            float totale;
+
+            foreach (string nazione in File.ReadLines(v))
+            {
+                campi = nazione.Split(' ');
+                totale = calcolaTotale(campi);
+
+                if (i == 0)
+                {
+                    nazioni[i].nome = campi[3];
+                    aus[i] = campi[3];
+                    nazioni[i++].punteggio = totale;
+                }
+                else if (Array.IndexOf(aus, campi[3]) == -1)
+                {
+                    aus[i] = campi[1];
+                    nazioni[i].punteggio = totale;
+                    nazioni[i++].nome = campi[3];
+                }
+            }
+
+            return i;
+        }
+        private int caricaCategorie(categoria[] categorie, string v)
+        {
+            string[] campi = new string[9], aus = new string[20];
+            int i = 0;
+            float totale;
+
+            foreach (string categoria in File.ReadLines(v))
+            {
+                campi = categoria.Split(' ');
+                totale = calcolaTotale(campi);
+
+                if (i == 0)
+                {
+                    categorie[i].tipo = campi[1];
+                    aus[i] = campi[1];
+                    categorie[i++].punteggio = totale;
+                }
+                else if (Array.IndexOf(aus, campi[1]) == -1)
+                {
+                    aus[i] = campi[1];
+                    categorie[i].punteggio = totale;
+                    categorie[i++].tipo = campi[1];
+                }
+            }
+
+            return i;
+        }
+        private float calcolaTotale(string[] campi)
+        {
+            float[] v = new float[5];
+            float max, min, totale = 0;
+
+            for (int i = 0; i < 5; i++)
+            {
+                v[i] = float.Parse(campi[i + 4]);
+            }
+
+            max = v.Max();
+            min = v.Min();
+
+            for (int i = 0; i < 5; i++)
+            {
+                totale += v[i];
+            }
+
+            return totale - max - min;
+        }
+    }
+}
